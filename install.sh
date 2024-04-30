ba6da7a646d2f90dff44490db7e3d05f48178add
Kito30 <duyanhpham30102004@gmail.com>
2024-04-30 12:32:08 +1000

    udate

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 ﻿
-yay -S google-chrome kitty waybar swww waypaper neofetch vesktop-bin steam btop qt6ct nwg-look onedrivegui qt6-wayland qt5-wayland polkit-kde-agent rofi-lbonn-wayland network-manager-applet blueman visual-studio-code-bin mpv spotify-launcher youtube-music-bin ark intel-media-driver jp2a fcitx5 fcitx5-qt fcitx5-gtk fcitx5-bamboo fcitx5-mozc fcitx5-configtool powertop winegui-bin github-cli fish libreoffice-fresh hyprlock tumbler breeze-icons nwg-displays wlr-rander hypridle udiskie cliphist nerd-fonts grimblast nautilus nodejs jdk-openjdk
+yay -S google-chrome kitty waybar swww waypaper neofetch vesktop-bin steam btop qt6ct nwg-look onedrivegui qt6-wayland qt5-wayland polkit-kde-agent rofi-lbonn-wayland-git network-manager-applet blueman visual-studio-code-bin mpv spotify-launcher ark intel-media-driver jp2a fcitx5 fcitx5-qt fcitx5-gtk fcitx5-bamboo fcitx5-mozc fcitx5-configtool powertop winegui-bin github-cli fish libreoffice-fresh hyprlock tumbler breeze-icons nwg-displays wlr-randr hypridle udiskie cliphist nerd-fonts flameshot-git nautilus nodejs jdk-openjdk
 
 sudo pacman -S hyprland sddm  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk  
 
