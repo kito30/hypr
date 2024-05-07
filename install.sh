0e6e706565d5a9a0c27ad1254290f1d6f6aa84fb
Kito30 <duyanhpham30102004@gmail.com>
2024-05-07 12:02:41 +1000

    updateconf

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 ﻿
-yay -S google-chrome kitty waybar swww waypaper neofetch vesktop-bin steam btop qt6ct nwg-look onedrivegui qt6-wayland qt5-wayland polkit-kde-agent rofi-lbonn-wayland-git network-manager-applet blueman visual-studio-code-bin mpv spotify-launcher ark intel-media-driver jp2a fcitx5 fcitx5-qt fcitx5-gtk fcitx5-bamboo fcitx5-mozc fcitx5-configtool powertop winegui-bin github-cli fish libreoffice-fresh hyprlock tumbler breeze-icons nwg-displays wlr-randr hypridle udiskie cliphist nerd-fonts flameshot-git nautilus nodejs jdk-openjdk
+yay -S google-chrome grim kitty-git waybar swww waypaper neofetch vesktop-bin steam btop qt6ct nwg-look onedrivegui qt6-wayland qt5-wayland polkit-gnome rofi-lbonn-wayland-git network-manager-applet blueman visual-studio-code-bin mpv spotify-launcher ark intel-media-driver jp2a fcitx5 fcitx5-qt fcitx5-gtk fcitx5-bamboo fcitx5-mozc fcitx5-configtool powertop winegui-bin github-cli fish libreoffice-fresh hyprlock tumbler breeze-icons nwg-displays wlr-randr hypridle udiskie cliphist nerd-fonts flameshot-git nautilus nodejs jdk-openjdk python-pywal
 
-sudo pacman -S hyprland sddm  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk  
+yay -S hyprland-git sddm  xdg-desktop-portal-hyprland-git xdg-desktop-portal-gtk-git
 
 sleep 5
 
