85dd40540d70321a68cf717bf46bf1c095831fab
kito30 <duyanhpham30102004@gmail.com>
2024-07-24 14:42:30 +1000

    update

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
-﻿yay -S brightnessctl wpgtk gthump google-chrome grim kitty-git waybar-git swww waypaper fastfetch vesktop-bin steam btop qt6ct nwg-look onedrivegui qt6-wayland qt5-wayland polkit-gnome rofi-lbonn-wayland-git network-manager-applet blueman visual-studio-code-bin mpv spotify-launcher ark intel-media-driver fcitx5 fcitx5-qt fcitx5-gtk fcitx5-bamboo fcitx5-mozc fcitx5-configtool powertop winegui-bin github-cli fish libreoffice-fresh hyprlock tumbler breeze-icons nwg-displays wlr-randr hypridle udiskie wl-clipboard nerd-fonts grim slurp copyq swappy thunar nodejs jdk-openjdk python-pywal npm
+﻿#!/bin/bash
 
-yay -S hyprland sddm xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+# Install packages
+yay -S --needed --noconfirm \
+    brightnessctl wpgtk gthump google-chrome grim kitty-git waybar-git swww waypaper fastfetch vesktop-bin steam btop qt6ct nwg-look onedrivegui qt6-wayland qt5-wayland polkit-gnome rofi-lbonn-wayland-git network-manager-applet blueman visual-studio-code-bin mpv spotify-launcher ark intel-media-driver fcitx5 fcitx5-qt fcitx5-gtk fcitx5-bamboo fcitx5-mozc fcitx5-configtool powertop winegui-bin github-cli fish libreoffice-fresh hyprlock tumbler breeze-icons nwg-displays wlr-randr hypridle udiskie wl-clipboard nerd-fonts grim slurp copyq swappy thunar nodejs jdk-openjdk python-pywal npm;
 
+# Wait for package installations to complete
 sleep 5
-wpg-install.sh -G 
-wpg-install.sh -r
-sleep 5
-systemctl enable bluetooth
-systemctl enable sddm
-systemctl start sddm
+
+# Install wpgtk themes and generate configurations
+wpg-install.sh -G;
+wpg-install.sh -r;
+
+cp -rf .config/ .fonts/ ~/.;
+# Enable and start services
+systemctl enable bluetooth;
+systemctl enable sddm;
+systemctl start sddm;
+
