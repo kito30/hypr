2bdfa0ff0a5224df145e793a75bfe2ffedbdf011
Kito30 <duyanhpham30102004@gmail.com>
2024-10-03 12:57:47 +1000

    sadsad

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Install packages
 yay -S --needed --noconfirm \
-    wl-clipboard pywal-discord-git brightnessctl catnap-git sddm xdg-desktop-portal-hyprland xdg-desktop-portal-gtk wpgtk gthumb google-chrome grim kitty waybar-git swww waypaper fastfetch vesktop-bin steam btop qt6ct nwg-look onedrivegui qt6-wayland qt5-wayland polkit-gnome rofi-lbonn-wayland-git network-manager-applet blueman visual-studio-code-bin mpv spotify-launcher ark fcitx5 fcitx5-qt fcitx5-gtk fcitx5-bamboo fcitx5-mozc fcitx5-configtool powertop winegui-bin github-cli fish libreoffice-fresh hyprlock tumbler breeze-icons nwg-displays wlr-randr hypridle udiskie wl-clipboard nerd-fonts grim slurp copyq swappy thunar nodejs jdk-openjdk python-pywal npm;
+    wl-clipboard pywal-discord-git brightnessctl catnap-git sddm xdg-desktop-portal-hyprland xdg-desktop-portal-gtk wpgtk gthumb google-chrome grim kitty-git waybar hyprpaper swww waypaper fastfetch vesktop-bin steam btop qt6ct nwg-look onedrivegui qt6-wayland qt5-wayland polkit-gnome rofi-wayland network-manager-applet blueman visual-studio-code-bin mpv spotify-launcher ark fcitx5 fcitx5-qt fcitx5-gtk fcitx5-bamboo fcitx5-mozc fcitx5-configtool powertop winegui-bin github-cli fish libreoffice-fresh hyprlock tumbler breeze-icons nwg-displays wlr-randr hypridle udiskie wl-clipboard nerd-fonts grim slurp copyq swappy thunar nodejs jdk-openjdk python-pywal npm;
 
 # Wait for package installations to complete
 sleep 5
