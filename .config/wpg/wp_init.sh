255d0679fd47d5cff88759524b87a8f3e0e29ab4
kito30 <duyanhpham30102004@gmail.com>
2025-01-13 14:32:48 +0700

    update

--- .config/wpg/wp_init.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-wpg -rs '/home/kito/OneDrive_Kito/animepic/GH51HzbagAAh58l.jpeg' '/home/kito/OneDrive_Kito/animepic/GH51HzbagAAh58l.jpeg'
--- install.sh
+++ install.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+#Install Hyprland essential
+yay -S --needed --noconfirm \
+   xdg-desktop-portal-hyprland hyprpaper hyprland hypridle hyprlock;
 # Install packages
 yay -S --needed --noconfirm \
-    wl-clipboard pywal-discord-git brightnessctl catnap-git sddm xdg-desktop-portal-hyprland xdg-desktop-portal-gtk wpgtk gthumb google-chrome grim kitty-git waybar hyprpaper swww waypaper fastfetch vesktop-bin steam btop qt6ct nwg-look onedrivegui qt6-wayland qt5-wayland polkit-gnome rofi-wayland network-manager-applet blueman visual-studio-code-bin mpv spotify-launcher ark fcitx5 fcitx5-qt fcitx5-gtk fcitx5-bamboo fcitx5-mozc fcitx5-configtool powertop winegui-bin github-cli fish libreoffice-fresh hyprlock tumbler breeze-icons nwg-displays wlr-randr hypridle udiskie wl-clipboard nerd-fonts grim slurp copyq swappy thunar nodejs jdk-openjdk python-pywal npm;
+   cmake meson cpio pkg-config brightnessctl catnap-git sddm xdg-desktop-portal-gtk wpgtk \
+   gthumb google-chrome kitty waybar swww waypaper fastfetch vesktop-bin btop qt6ct \
+   nwg-look onedrivegui qt6-wayland polkit-gnome rofi-wayland network-manager-applet blueman \
+   visual-studio-code-bin mpv ark fcitx5 fcitx5-qt fcitx5-gtk fcitx5-bamboo fcitx5-mozc \
+   fcitx5-configtool powertop winegui-bin github-cli fish libreoffice-fresh tumbler udiskie \
+   nerd-fonts thunar nodejs jdk-openjdk python-pywal16 npm;
 
 # Wait for package installations to complete
 sleep 5
 
+#Screenshot utility
+yay -S --needed --noconfirm \
+   wl-clipboard grim slurp cliphist swappy;
+
+yay -S steam;
 # Install wpgtk themes and generate configurations
 wpg-install.sh -G;
 wpg-install.sh -r;
+wpg-install.sh -i;
 
 cp -rf .config/ .fonts/ ~/.;
 # Enable and start services
