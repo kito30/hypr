b0a51749182a262de587e462aa0dd780f6b90e77
kito30 <duyanhpham30102004@gmail.com>
2025-08-22 14:46:43 +1000

    Add large files using Git LFS

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
+    starship init fish | source
+
 end
+
+# Created by `pipx` on 2025-08-03 08:20:50
+set PATH $PATH /home/kito/.local/bin
--- install.sh
+++ install.sh
@@ -1,32 +1,73 @@
 #!/bin/bash
+set -e  # exit if any command fails
 
-#Install Hyprland essential
+### ---------------------------------------------------------
+### Install Hyprland essentials
+### ---------------------------------------------------------
 yay -S --needed --noconfirm \
-   xdg-desktop-portal-hyprland hyprland hypridle hyprlock;
-# Install packages
-yay -S --needed --noconfirm \
-   cmake meson cpio pkg-config brightnessctl catnap-git sddm xdg-desktop-portal-gtk wpgtk \
-   gthumb google-chrome kitty ags-hyprpanel-git fcitx5-material-color waypaper matugen swww fastfetch vesktop-bin btop qt6ct \
-   nwg-look nwg-displays onedrivegui qt6-wayland polkit-gnome rofi-wayland network-manager-applet blueman \
-   visual-studio-code-bin mpv ark fcitx5 fcitx5-qt fcitx5-gtk fcitx5-bamboo fcitx5-mozc \
-   fcitx5-configtool powertop winegui-bin github-cli fish libreoffice-fresh tumbler udiskie \
-   nerd-fonts thunar nodejs jdk-openjdk python-pywal16 npm gvfs-mtp;
+  xdg-desktop-portal-hyprland \
+  hyprland \
+  hypridle \
+  hyprlock
 
-# Wait for package installations to complete
+### ---------------------------------------------------------
+### Install general packages
+### ---------------------------------------------------------
+yay -S --needed --noconfirm \
+  cmake meson cpio pkg-config brightnessctl catnap-git sddm \
+  xdg-desktop-portal-gtk wpgtk gthumb google-chrome kitty \
+  ags-hyprpanel-git fcitx5-material-color waypaper matugen swww \
+  fastfetch vesktop-bin btop nwg-look nwg-displays onedrivegui \
+  qt6-wayland polkit-kde-agent gnome-keyring rofi-wayland \
+  network-manager-applet bluedevil visual-studio-code-bin mpv \
+  fcitx5 fcitx5-qt fcitx5-gtk fcitx5-bamboo fcitx5-mozc \
+  fcitx5-configtool winegui-bin github-cli fish libreoffice-fresh \
+  tumbler udiskie nerd-fonts nodejs jdk-openjdk python-pywal16 npm \
+  gvfs-mtp dolphin xdg-desktop-portal-kde systemsettings \
+  sddm-kcm plasma-thunderbolt print-manager plasma5support \
+  plasma-integration
+
+# Give system a little break after installs
 sleep 5
 
-#Screenshot utility
+### ---------------------------------------------------------
+### Screenshot utilities
+### ---------------------------------------------------------
 yay -S --needed --noconfirm \
-   wl-clipboard grim slurp cliphist swappy;
+  wl-clipboard grim slurp cliphist swappy
 
-yay -S steam;
-# Install wpgtk themes and generate configurations
-wpg-install.sh -G;
-wpg-install.sh -r;
-wpg-install.sh -i;
+### ---------------------------------------------------------
+### Extra apps
+### ---------------------------------------------------------
+yay -S --needed --noconfirm steam
 
-cp -rf .config/ .fonts/ ~/.;
-# Enable and start services
-systemctl enable bluetooth;
-systemctl enable sddm;
-systemctl start sddm;
+### ---------------------------------------------------------
+### Setup wpgtk themes
+### ---------------------------------------------------------
+wpg-install.sh -G
+wpg-install.sh -r
+wpg-install.sh -i
+
+### ---------------------------------------------------------
+### Copy user configs
+### ---------------------------------------------------------
+cp -rf .config/ .fonts/ ~/
+
+### ---------------------------------------------------------
+### Setup SDDM theme
+### ---------------------------------------------------------
+# If you have sddm.zip instead of a folder, uncomment these:
+# unzip -o sddm.zip -d /tmp/sddm
+# sudo cp -rf /tmp/sddm/* /usr/share/sddm/themes/silent/
+# rm -rf /tmp/sddm
+
+# If you already have an "sddm/" folder in the repo:
+sudo mkdir -p /usr/share/sddm/themes/silent
+sudo cp -rf sddm/* /usr/share/sddm/themes/silent/
+
+### ---------------------------------------------------------
+### Enable & start services
+### ---------------------------------------------------------
+sudo systemctl enable bluetooth
+sudo systemctl enable sddm
+sudo systemctl start sddm
